ab3773b031f42fd0f5e29da3996348456a2a23cd
LvQiang <lq15512728505@163.com>
2020-11-12 18:45:52 +0800

    Initial Commit

---
+++ JLLogDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  JLLogDemo
+//
+//  Created by John Lyn on 2020/11/12.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ JLLogDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  JLLogDemo
+//
+//  Created by John Lyn on 2020/11/12.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
